5743df4684322d24590162d9fd55fc5ce321c0a8
Rolando Lucio <rolandolucio@gmail.com>
2016-01-14 17:44:12 -0600

    tables squema

--- src/Compropago/Utils/Store.php
+++ src/Compropago/Utils/Store.php
@@ -66,4 +66,51 @@ class Store{
 		// Ok Move on
 		return true;
 	}
+
+	/**
+	 * SQL query for Droping ComproPago Tables
+	 * @return string[]
+	 * @since 1.0.2
+	 */
+	public static function sqlDropTables($prefix=null){
+		return array(
+				'DROP TABLE IF EXISTS `' . $prefix . 'compropago_orders`;',
+				'DROP TABLE IF EXISTS `' . $prefix . 'compropago_transactions`;'
+		);
+	}
+	/**
+	 * SQL query for Creating ComproPago Tables
+	 * @return string[]
+	 * @since 1.0.2
+	 */
+	public static function sqlCreateTables($prefix=null){
+		return array(
+				'CREATE TABLE `' . $prefix . 'compropago_orders` (
+			`id` int(11) NOT NULL AUTO_INCREMENT,
+			`date` int(11) NOT NULL,
+			`modified` int(11) NOT NULL,
+			`compropagoId` varchar(50) NOT NULL,
+			`compropagoStatus`varchar(50) NOT NULL,
+			`storeCartId` varchar(255) NOT NULL,
+			`storeOrderId` varchar(255) NOT NULL,
+			`storeExtra` varchar(255) NOT NULL,
+			`ioIn` mediumtext,
+			`ioOut` mediumtext,
+			PRIMARY KEY (`id`), UNIQUE KEY (`compropagoId`)
+	
+			)ENGINE=MyISAM DEFAULT CHARSET=utf8  DEFAULT COLLATE utf8_general_ci  AUTO_INCREMENT=1 ;',
+					
+				'CREATE TABLE `' . $prefix . 'compropago_transactions` (
+			`id` int(11) NOT NULL AUTO_INCREMENT,
+			`orderId` int(11) NOT NULL,
+			`date` int(11) NOT NULL,
+	 		`compropagoId` varchar(50) NOT NULL,
+			`compropagoStatus` varchar(50) NOT NULL,
+			`compropagoStatusLast` varchar(50) NOT NULL,
+			`ioIn` mediumtext,
+			`ioOut` mediumtext,
+			PRIMARY KEY (`id`)
+			)ENGINE=MyISAM DEFAULT CHARSET=utf8  DEFAULT COLLATE utf8_general_ci  AUTO_INCREMENT=1 ;'
+		);
+	}
 }
