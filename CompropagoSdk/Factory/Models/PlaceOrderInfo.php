7b4c872af8fbec8859f43410d709046c115e9c5f
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2016-12-27 18:00:43 -0600

    Finish refactor, init testing

--- CompropagoSdk/Factory/Models/PlaceOrderInfo.php
+++ CompropagoSdk/Factory/Models/PlaceOrderInfo.php
@@ -1,33 +1,9 @@
 <?php
-/**
- * Copyright 2015 Compropago.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * Compropago php-sdk
- * @author Eduardo Aguilar <eduardo.aguilar@compropago.com>
- */
 
-
-namespace CompropagoSdk\Models;
+namespace CompropagoSdk\Factory\Models;
 
 use CompropagoSdk\Client;
 
-/**
- * Class PlaceOrderInfo informacion de nuevas ordenes
- * @package CompropagoSdk\Models
- */
 class PlaceOrderInfo
 {
     public $order_id;
@@ -36,6 +12,7 @@ class PlaceOrderInfo
     public $customer_name;
     public $customer_email;
     public $payment_type;
+    public $currency;
     public $image_url;
     public $app_client_name;
     public $app_client_version;
@@ -46,7 +23,8 @@ class PlaceOrderInfo
         $order_price, 
         $customer_name, 
         $customer_email, 
-        $payment_type="OXXO", 
+        $payment_type="OXXO",
+        $currency="MXN",
         $image_url=null, 
         $app_client_name="phpsdk", 
         $app_client_version=Client::VERSION
@@ -58,6 +36,7 @@ class PlaceOrderInfo
         $this->customer_name      = $customer_name;
         $this->customer_email     = $customer_email;
         $this->payment_type       = $payment_type;
+        $this->currency           = $currency;
         $this->image_url          = $image_url;
         $this->app_client_name    = $app_client_name;
         $this->app_client_version = $app_client_version;
