c805c8ee976759c69d85b1630c02f459097d974c
Eduardo Aguilar <dante.aguilar41@gmail.com>
2017-08-28 20:58:44 -0500

    Refactor Class properties

--- CompropagoSdk/Factory/Models/OrderInfo.php
+++ CompropagoSdk/Factory/Models/OrderInfo.php
@@ -7,7 +7,12 @@ class OrderInfo
     public $order_id;
     public $order_name;
     public $order_price;
+    public $payment_method;
+    public $store;
+    public $country;
     public $image_url;
+    public $success_url;
+    public $failed_url;
     public $exchage;
 
     public function __construct()
